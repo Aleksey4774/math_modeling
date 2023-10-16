47ac54279273f2259cc62c6971994e3d61d3aded
Aleksey4774 <144795422+Aleksey4774@users.noreply.github.com>
2023-10-16 22:12:44 +0200

    ПРП
    ПРП

--- Lab_2_task2.py
+++ Lab_2_task2.py
@@ -1,1 +1,9 @@
 a = int(input("Введите первый член: "))
+b = int(input("Введите знаменатель прогрессии: "))
+c = int(input("Введите количество членов: "))
+
+chisl = a*((b**c)-1)
+znam = b - 1
+otv = chisl / znam
+
+print(otv)
