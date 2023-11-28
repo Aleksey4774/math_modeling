4d538d40c4800a747ba295a25ef3f5de69e9cbc3
Aleksey4774 <144795422+Aleksey4774@users.noreply.github.com>
2023-11-28 13:15:13 +0200

    28.11
    1 xfcnm

--- Lab_3_task2.py
+++ Lab_3_task2.py
@@ -1,8 +1,7 @@
 import numpy as np 
 
 
-U =  np.sqrt(g*h*tan(2)**2 / 2*cos(1)**2 *(1 - tan(2)*tan(2)))
-
+U =  np.sqrt(g*h*tan(2)**2 / 2*cos(1)**2 *(1 - tan(2)*tan(2))) 
 
 
 
--- Lab_3_task3.py
+++ Lab_3_task3.py
@@ -1,10 +1,19 @@
 import numpy as np 
+import Lab_3_task1
+x0 = 0
+y0 = 0
+v0x = 0
+g = Lab_3_task1.gravity_constant
 
-t = np.arrange(0, 5, 0,1)
+t = np.arange(0.5, 0.1)
 x = x0 + v0x*t 
 y = x0 + v0x*t - g * t ** 2 / 2
 
+a = [t, x, y]
+b = np.array(a)
 
+print(type(a))
+print(type(b))
 
 
 
