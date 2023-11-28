e4b9e1f52e693d1b310dab8eecfa5534d334e398
Aleksey4774 <144795422+Aleksey4774@users.noreply.github.com>
2023-11-28 14:27:19 +0200

    2811
    2 xfcnm

---
+++ Lec_4_cool_func.py
@@ -0,0 +1,15 @@
+from Lec_4_create_fanc import mult_func
+from Lec_4_create_fanc import my_print
+
+tmp = mult_func(4)
+
+print(tmp)
+
+print(mult_func(10))
+
+print(mult_func('Good'))
+
+	
+my_print('Hello')
+ 
+my_print(mult_func('50'))
---
+++ Lec_4_scope.py
@@ -0,0 +1,16 @@
+x0 = 10
+
+def move(t):
+    x = x0 * t
+    return x
+
+print(move(3))
+#print(x)
+a = "GOOD"
+
+def my_func():
+    a = "BAD"
+    print(a)
+
+my_func()    
+print(a)
