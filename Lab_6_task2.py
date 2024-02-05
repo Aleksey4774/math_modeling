977551761ce3c9af472c9a2a8e72c840bf495277
Aleksey4774 <144795422+Aleksey4774@users.noreply.github.com>
2024-02-05 18:33:56 +0200

    12121
    121212

---
+++ Lab_6_task2.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def giperbola_plotter (k = 1):
+
+    x = np.arange(-10, 10, 1)
+    y = k / x
+
+    plt.plot(x, y)
+    plt.savefig('fig_task2.png')
+
+if __name__ == '__main__':
+    giperbola_plotter()
