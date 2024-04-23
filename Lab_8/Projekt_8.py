8331ce05658ff61120c6567ac15414db9dc9dbaa
Aleksey4774 <144795422+Aleksey4774@users.noreply.github.com>
2024-04-23 14:08:04 +0200

    23/04
    23/04

---
+++ Lab_8/Projekt_8.py
@@ -0,0 +1,45 @@
+import matplotlib. pyplot as plt 
+import numpy as np 
+from matplotlib. animation import FuncAnimation
+import warnings
+import matplotlib
+warnings.filterwarnings("ignore", category=matplotlib.MatplotlibDeprecationWarning)
+
+def planet(R, angle_vel, time):
+    alpha = angle_vel * np.pi / 180 * time
+    x = R * np.cos(alpha)
+    y = R * np.sin(alpha) * 0.8
+    return x, y
+
+def moonplt(R, vel, t):
+    x0, y0 = (planet(R =5.5, vel =1, t=t))
+    alpha = vel * np.pi / 180 * t 
+    x = x0 + R*np.cos(alpha) * 0,8
+    y0 = y0 + R*np.sin(alpha) * 0,8
+    return x, y 
+
+def animate(i):
+    earth.set_data(planet(R=10, angle_vel=1, time=i))
+    moon.set_data(moonplt(R=1.2, vel=365, t=i))
+
+    ax.set_title("Движение вокруг солнца")
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    plt.grid()
+    plt.plot([0], [0], "o", color = "orange", ms = 10)
+    earth, = plt.plot([], [], 'o', color='blue', label='Ball')
+    moon, = plt.plot([], [], 'o', ms=3, color='grey')
+    
+
+    edge = 12
+
+    ax.set_xlim(-edge, edge)
+    ax.set_ylim(-edge, edge)
+
+    ani = FuncAnimation(fig,
+                        animate,
+                        frames=360,
+                        interval=30)
+    
+    ani.save('project.gif') 
